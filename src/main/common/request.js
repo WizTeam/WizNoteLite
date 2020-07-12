1d353a798e72fe4e6aec36db5dcf84e4d318b3da
wizweishijun <weishijun@wiz.cn>
2020-07-12 09:48:02 +0800

    尝试解决苹果审核无法解析as.wiz.cn域名的问题

--- src/main/common/request.js
+++ src/main/common/request.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const assert = require('assert');
 const i18next = require('i18next');
+const URL = require('url');
+
 const { WizNetworkError, WizInternalError, WizKnownError } = require('../../share/error');
 
 //
@@ -54,6 +56,21 @@ async function standardRequest(opt) {
     return data.result;
   } catch (err) {
     if (err.code === 'ENOTFOUND') {
+      //
+      // 尝试解决苹果审核无法解析as.wiz.cn域名的问题
+      if (err.hostname === 'as.wiz.cn') {
+        const url = URL.parse(options.url);
+        options.headers = options.headers || {};
+        options.headers.Host = url.hostname; // put original hostname in Host header
+        url.hostname = '120.55.138.92';
+        delete url.host; // clear hostname cache
+        options.url = URL.format(url);
+        //
+        const ret = await standardRequest(options);
+        return ret;
+      }
+      //
+      //
       throw new WizNetworkError(i18next.t('errorConnect', {
         host: err.hostname,
       }));
