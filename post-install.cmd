2e0c782ca192540bd8ba1a03315cdbabb166e017
steve.wei <wsj@wiz.cn>
2021-05-13 11:27:34 +0800

    样式

--- post-install.cmd
+++ post-install.cmd
@@ -1,3 +1,1 @@
-# Xcopy /E /I /Q /Y .\node_modules\wiz-vditor\dist .\public\libs\wiz-vditor\dist\
-
 .\node_modules\.bin\electron-rebuild.cmd
--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,1 @@
 ./node_modules/.bin/electron-rebuild
-# mkdir -p public/libs/wiz-vditor 
-# cp -rf ./node_modules/wiz-vditor/dist ./public/libs/wiz-vditor
